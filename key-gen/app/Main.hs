b90f43dfef0bbe48783c2131ed7fca25c1fc30db
rjlacanlaled <rjlacanlaled@gmail.com>
2023-02-18 00:44:30 +0800

    add logs

--- key-gen/app/Main.hs
+++ key-gen/app/Main.hs
@@ -11,5 +11,8 @@ main = do
     let secretPath = args !! 0
         skeyPath   = args !! 1
         pass       = T.pack $ args !! 2
+    putStrLn secretPath
+    putStrLn skeyPath
+    putStrLn pass
     importTrustStoreFromCardano @_ @C.PaymentKey C.AsPaymentKey (SecretFile secretPath) skeyPath (KeyPass pass)
     pure ()
